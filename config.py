a5a85575760d9c8a3f545a408028f46e89d69294
Wertheins <lucaswerthaine@gmail.com>
2025-08-06 14:08:51 -0300

    modificado para se adequar ao streamlit

--- config.py
+++ config.py
@@ -1,16 +1,27 @@
 import gspread
+import streamlit as st
 from dotenv import load_dotenv
+import json
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
 try:
-    gc = gspread.service_account(filename='credentials.json')
+    # Verifica se está rodando no ambiente do Streamlit e usa os secrets
+    if "GCP_CREDENTIALS" in st.secrets:
+        creds_json = st.secrets["GCP_CREDENTIALS"]
+        creds_dict = json.loads(creds_json)
+        gc = gspread.service_account_from_dict(creds_dict)
+    else:
+        # Se não, usa o arquivo local (para rodar na sua máquina)
+        gc = gspread.service_account(filename='credentials.json')
+
     spreadsheet = gc.open("teste")
-    worksheet = spreadsheet.worksheet("Sheet2") 
-    print("Conexão com a planilha 'teste' e aba 'Sheet2' bem-sucedida.")
+    worksheet = spreadsheet.worksheet("Sheet2")
+    print("Conexão com a planilha bem-sucedida.")
 except Exception as e:
     print(f"Erro fatal ao conectar com o Google Sheets: {e}")
+    st.error(f"Erro de conexão com a base de dados: {e}")
     exit()
 
 # Adicionamos uma "área de preparação" para os produtos encontrados
